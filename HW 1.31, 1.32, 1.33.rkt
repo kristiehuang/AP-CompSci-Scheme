ffc10a44b3e12c52007d50ef0adb01039979fa25
Kristie Huang <kristiehuang01@gmail.com>
2017-09-08 22:15:27 -0700

    9/8 Update

--- HW 1.31, 1.32, 1.33.rkt
+++ HW 1.31, 1.32, 1.33.rkt
@@ -1,8 +1,41 @@
+(require (lib "trace.ss"))
+
 ;1.30
+;linear recursion
+(define (next a) (+ 1 a))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b)))
+  )
+
+
+;iterative
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter <??> <??>)))
+  
+  (iter <??> <??>)
+  )
 
 ;1.31 a
 
-;1.32 iterative
+; procedural abstraction - generalizes PROCEDURES
+
+
+
+
+
+;1.32 iterative only
+(define (accumulate op op-null term a next b)
+(if ((< a b) op-null)
+      (op (term a)
+         (accumulate term (next a) next b))))
+  )
 
 ;1.33 (part a, use odd numbers)
 (define (gcd a b)
