f2f3efcd1492b3a58206ece5aae54f3942bf784b
Kristie Huang <kristiehuang01@gmail.com>
2017-10-09 10:53:53 -0700

    10/9 - Unit 2

---
+++ Notes/2Abstraction.rkt
@@ -0,0 +1,21 @@
+;abstraction
+
+;most fundamental data structure
+; pair
+; list is just a bunch of pairs
+
+; constructor
+cons
+; selectors (accessories); 
+cdr, car,
+predicates
+
+; mutator (modifier)
+
+; constants
+null
+
+make-rat using gcd
+mul-rat
+numer
+denom
