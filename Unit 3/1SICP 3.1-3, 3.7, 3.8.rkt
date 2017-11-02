4d20349047338fec8e43f547e60e77850a2b8aac
Kristie Huang <kristiehuang01@gmail.com>
2017-11-02 13:14:25 -0700

    11/2 update

--- Unit 3/1SICP 3.1-3, 3.7, 3.8.rkt
+++ Unit 3/1SICP 3.1-3, 3.7, 3.8.rkt
@@ -21,12 +21,62 @@
   )
  
 
-
-
 ;3.3
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  
+  (define (dispatch m)
+    (cond ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MAKE-ACCOUNT"
+                       m))))
+
+  (lambda (p m) (if (eq? p password) (dispatch m) (error "incorrect password")))
+  )
+
+
 
 
 ;3.7
 
+(define (make-joint acc pass newpass)
+  (cond
+    ;if acc w/ pass exists, then do that. if not then u cant access
+    ((number? ((acc pass 'deposit) 0))
+     (lambda (enter-pass m) (if (eq? enter-pass newpass) (acc pass m) (error "incorrect pass"))))
+    (else (error "u cant access this acc sry")))
+  )
 
-;3.8
+(define peter-acc (make-account 50 'open-sesame))
+
+(define paul-acc
+  (make-joint peter-acc 'open-sesame 'rosebud))
+
+;((peter-acc 'pass 'deposit) 50)
+;((paul-acc 'newpass 'deposit) 50)
+
+
+;3.8 broken
+
+(define (f num)
+  (define counter 0)
+
+
+  
+  (if (= counter 0)     
+      (begin 0   (if (= num 0) (set! counter 3)
+      (set! counter (+ 1 counter))))
+      1)
+  ;if num=1 has been evaluated before, then return 1. if not, return 0
+  
+  )
+(+ (f 0) (f 1)) ; 0
+(+ (f 1) (f 0)) ; 1
